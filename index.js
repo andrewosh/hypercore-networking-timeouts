a374794f69677f7a9ab03eb0442050736fc502b8
Andrew Osheroff <andrewosh@gmail.com>
2020-08-12 17:27:28 +0200

    Extracted the wrong function

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
-module.exports = function setNetworkingTimeouts (networker, core) {
-  const discoveryKey = core.discoveryKey
-  const timeouts = {
-    get: (cb) => {
-      if (networker.joined(discoveryKey)) {
-        if (networker.flushed(discoveryKey)) return cb()
+module.exports =  function registerCoreTimeouts (networker, corestore) {
+  const flushSets = new Map()
+
+  networker.on('flushed', dkey => {
+    const keyString = dkey.toString('hex')
+    if (!flushSets.has(keyString)) return
+    const { flushSet, peerAddSet } = flushSets.get(keyString)
+    callAllInSet(flushSet)
+    callAllInSet(peerAddSet)
+  })
+
+  corestore.on('feed', core => {
+    const discoveryKey = core.discoveryKey
+    const peerAddSet = new Set()
+    const flushSet = new Set()
+    var globalFlushed = false
+
+    if (!networker.swarm || networker.swarm.destroyed) return
+    networker.swarm.flush(() => {
+      if (networker.joined(discoveryKey)) return
+      globalFlushed = true
+      callAllInSet(flushSet)
+      callAllInSet(peerAddSet)
+    })
+
+    flushSets.set(discoveryKey.toString('hex'), { flushSet, peerAddSet })
+    core.once('peer-add', () => {
+      callAllInSet(peerAddSet)
+    })
+
+    const timeouts = {
+      get: (cb) => {
+        if (networker.joined(discoveryKey)) {
+          if (networker.flushed(discoveryKey)) return cb()
+          return flushSet.add(cb)
+        }
+        if (globalFlushed) return cb()
         return flushSet.add(cb)
-      }
-      if (globalFlushed) return cb()
-      return flushSet.add(cb)
-    },
-    update: (cb) => {
-      const oldCb = cb
-      cb = (...args) => {
-        oldCb(...args)
-      }
-      if (core.peers.length) return cb()
-      if (networker.joined(discoveryKey)) {
-        if (networker.flushed(discoveryKey) && !core.peers.length) return cb()
+      },
+      update: (cb) => {
+        const oldCb = cb
+        cb = (...args) => {
+          oldCb(...args)
+        }
+        if (core.peers.length) return cb()
+        if (networker.joined(discoveryKey)) {
+          if (networker.flushed(discoveryKey) && !core.peers.length) return cb()
+          return peerAddSet.add(cb)
+        }
+        if (globalFlushed) return cb()
         return peerAddSet.add(cb)
       }
-      if (globalFlushed) return cb()
-      return peerAddSet.add(cb)
     }
+    core.timeouts = timeouts
+  })
+}
+
+function callAllInSet (set) {
+  for (const cb of set) {
+    cb()
   }
-  core.timeouts = timeouts
+  set.clear()
 }
